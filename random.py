ccadddf19a6fe26053332c27c5013ea6f991a46f
coder1-ops <64868397+coder1-ops@users.noreply.github.com>
2020-05-09 06:06:24 +0530

    Create random.py

---
+++ random.py
@@ -0,0 +1,15 @@
+import random
+
+random.randint(1,100)
+#generates random no from 1 to 100
+
+random.randrange(1,10,2)
+#generates a random no with step size 2 from 1 to 10 excluding 10
+
+random.random()
+#generates a random float no between 0 and 1
+
+random.choice([list])
+#returns an element from list
+
+''.join(random.sample(word,len(word)))
